00e9450a7026d5da7ef94b3f09424dca7061a0d9
Myeongzip <shayne0914@skuniv.ac.kr>
2023-09-22 15:11:51 +0900

    create article model and static file setting for image

--- articles/models.py
+++ articles/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from users.models import User
 
 # Create your models here.
+
+class Article(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=50)
+    content = models.TextField()
+    image = models.ImageField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)    # auto_created= -> 이 필드가 자동으로 생성되었는지에 대한 옵션? 
+    
+    def __str__(self):
+        return str(self.title)
--- articles/views.py
+++ articles/views.py
@@ -1,3 +1,43 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status, permissions
 
-# Create your views here.
+class ArticleView(APIView):    # /articles/
+    def get(self, request):    # list READ
+        pass
+    
+    def post(self, request):    # CREATE
+        pass
+        
+class ArticleDetailView(APIView): # /articles/{article_id}
+    def get(self, request, article_id):     # detail READ
+        pass
+    
+    def put(self, request, article_id):    #  detail UPDATE
+        pass
+    
+    def delete(self, request, article_id):    #  detail DELETE
+        pass
+    
+    
+
+class CommentView(APIView):    # /comment/
+    def get(self, request):    # comments of certain article, list READ
+        pass
+    
+    def post(self, request):    # CREATE
+        pass
+    
+    
+
+class CommentDetailView(APIView): # /comment/{comment_id}
+    def put(self, request, article_id):    #  comment_detail UPDATE
+        pass
+    
+    def delete(self, request, article_id):    #  comment_detail DELETE
+        pass
+    
+    
+class LikeView(APIView):    # /like/
+    def post(self, request):    # like
+        pass
