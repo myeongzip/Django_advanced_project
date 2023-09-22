41e069fbe14c614202cd41d69bcdce839b5666bf
Myeongzip <shayne0914@skuniv.ac.kr>
2023-09-22 11:16:17 +0900

    feat/signin and authorization permissions and startapp articles

--- users/serializers.py
+++ users/serializers.py
@@ -1,5 +1,7 @@
 from rest_framework import serializers
 from users.models import User
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
@@ -8,9 +10,26 @@ class UserSerializer(serializers.ModelSerializer):
     
     def create(self, validated_data):
         user = super().create(validated_data)   # DB에 연결
-        print(validated_data)
-        
         password = user.password
         user.set_password(password) # 비밀번호 해싱 but 이 자체로는 DB에 연결되지 않아 DB엔 해싱된 비밀번호가 들어가지 않음
         user.save() # DB에 전달하고 마무리하기 위한 save
         return user
+    
+    def update(self, validated_data):
+        user = super().create(validated_data)   # DB에 연결
+        password = user.password
+        user.set_password(password) # 비밀번호 해싱 but 이 자체로는 DB에 연결되지 않아 DB엔 해싱된 비밀번호가 들어가지 않음
+        user.save() # DB에 전달하고 마무리하기 위한 save
+        return user
+    
+
+
+class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
+    @classmethod
+    def get_token(cls, user):
+        token = super().get_token(user) # serializer 회원가입 create 만들 때랑 비슷한 형태
+
+        # Add custom claims, jwt decoded-payload 부분
+        token['email'] = user.email
+
+        return token
--- users/views.py
+++ users/views.py
@@ -1,15 +1,26 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-
-from users.serializers import UserSerializer
+from rest_framework import permissions
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+)
+from users.serializers import CustomTokenObtainPairSerializer, UserSerializer
 
 class UserView(APIView):
-    def post(self, request):
-        serializer = UserSerializer(data = request.data) # 꼭 data = request.data 지정해줘야 함.
+    def post(self, request):    # signup
+        serializer = UserSerializer(data = request.data) # method가 post 일 때 data = request.data 지정해줘야 함.
         if serializer.is_valid():
             serializer.save()
-            return Response({"message":"가입완료!"}, status=status.HTTP_201_CREATED)    # 저장한 데이터들 보여주기
+            return Response({"message":"가입완료!"}, status=status.HTTP_201_CREATED)
         else:
-            print(serializer.errors)        # 개발 단계에선 error의 정보를 알면 좋지만, 실제에선 쓰면 X
             return Response({"message":f"{serializer.errors}"}, status=status.HTTP_400_BAD_REQUEST) # 요청이 잘못됐기에 400이 맞음.
+        
+        
+class CustomTokenObtainPairView(TokenObtainPairView):   # signin/login
+    serializer_class = CustomTokenObtainPairSerializer
+    
+class mockView(APIView):
+    permission_classes = [permissions.IsAuthenticated] # 어떤 것들이 되는지 지정을 해줘야하는데, 그를 위해선 rest_framework의 permissions를 임포트해야됨.
+    def get(self, request):
+        return Response("get 요청!")
