194ef50f76affb73acd2de820d04feeda9c6fbac
Myeongzip <shayne0914@skuniv.ac.kr>
2023-09-24 22:48:18 +0900

    feat/follow and change detail page of serializer(incomplete)

--- articles/serializers.py
+++ articles/serializers.py
@@ -4,9 +4,14 @@ from articles.models import Comment
 
 
 class CommentSerializer(serializers.ModelSerializer):
+    user = serializers.SerializerMethodField()   # comment의 user의 id값을 보여주는 것이 디폴트인데, 보여주는 값을 email로 바꾸고 싶을 때
+    
+    def get_user(self, obj):
+        return obj.user.email
+
     class Meta:
         model = Comment
-        fields = "__all__"
+        exclude = ("article",)
         
 class CommentCreateSerializer(serializers.ModelSerializer):
     class Meta:
@@ -16,6 +21,8 @@ class CommentCreateSerializer(serializers.ModelSerializer):
 
 class ArticleSerializer(serializers.ModelSerializer):
     user = serializers.SerializerMethodField()
+    comment_set = CommentSerializer(many=True)  # 정확하게 'comment_set'로 해야 제대로 작동됨. related_name을 지정해줬다면 그걸로 해야 함.
+    likes = serializers.StringRelatedField(many=True)   # user의 string field로 나오게 하는 메소드(?)
     
     def get_user(self, obj):
         return obj.user.email
--- users/views.py
+++ users/views.py
@@ -5,7 +5,10 @@ from rest_framework import permissions
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
 )
+from articles.models import Article
+from users.models import User
 from users.serializers import CustomTokenObtainPairSerializer, UserSerializer
+from rest_framework.generics import get_object_or_404 
 
 class UserView(APIView):
     def post(self, request):    # signup
@@ -24,3 +27,14 @@ class mockView(APIView):
     permission_classes = [permissions.IsAuthenticated] # 어떤 것들이 되는지 지정을 해줘야하는데, 그를 위해선 rest_framework의 permissions를 임포트해야됨.
     def get(self, request):
         return Response("get 요청!")
+    
+class FollowView(APIView):  # 좋아요와 로직이 비슷함. -> 좋아요 view 긁어서 약간 변형
+    def post(self, request, user_id):
+        you = get_object_or_404(User, id=user_id)
+        me = request.user
+        if me in you.followers.all():
+            you.followers.remove(me)
+            return Response("unfollow 합니다.", status=status.HTTP_200_OK)
+        else:
+            you.followers.add(me)
+            return Response("follow 합니다", status=status.HTTP_200_OK)
